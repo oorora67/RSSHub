6e382d4f64808e120fcd98723ebbdc1897721721
oorora <oorora0067@gmail.com>
2026-03-20 20:41:03 +0900

    sitedel

--- lib/routes/lephoceen/namespace.ts
+++
@@ -1,7 +0,0 @@
-import type { Namespace } from '@/types';
-
-export const namespace: Namespace = {
-    name: 'Le Phocéen',
-    url: 'lephoceen.fr',
-    description: "Actualités de l'Olympique de Marseille du site lephocéen.fr",
-};
